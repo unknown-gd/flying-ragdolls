b077559ce57bb7239541e15d46cd443e2c452026
PrikolMen <olegis019@gmail.com>
2023-05-20 18:56:21 +0400

    More relevant code

--- lua/autorun/client/cl_flying_ragdolls.lua
+++ lua/autorun/client/cl_flying_ragdolls.lua
@@ -1,77 +1,77 @@
-local cvars_AddChangeCallback = cvars.AddChangeCallback
+local addonName = "Flying Ragdolls!"
+
+-- Libraries
+local hook = hook
+
+-- Variables
+local KEY_W, KEY_S, KEY_D, KEY_A = KEY_W, KEY_S, KEY_D, KEY_A
 local CreateClientConVar = CreateClientConVar
 local LocalPlayer = LocalPlayer
 local util_Decal = util.Decal
-local tonumber = tonumber
-local hook_Add = hook.Add
 local IsValid = IsValid
-local tobool = tobool
-local pairs = pairs
+local ipairs = ipairs
 
-local enable = CreateClientConVar( "flying_ragdolls", "1", true, false, "Enable/Disable ragdoll fly on client", 0, 1):GetBool()
-cvars_AddChangeCallback( "flying_ragdolls", function( name, old, new)
-    enable = tobool( new ) or false
-end, "flying_ragdolls")
-
-local velocity = (CreateClientConVar( "flying_ragdolls_speed", "10", true, false, "Ragdoll fly speed...", 0, 10000):GetInt() or 10) * 100
-cvars_AddChangeCallback( "flying_ragdolls_speed", function( name, old, new)
-    velocity = (tonumber( new ) or 10) * 100
-end, "flying_ragdolls")
+local enabled = CreateClientConVar( "cl_flying_ragdolls", "1", true, false, "Allows control of the player's body after death if 1.", 0, 1 )
+local blood = CreateClientConVar( "cl_flying_ragdolls_blood", "1", true, false, "If the impact is strong enough it will leave a trail of blood if 1.", 0, 1 )
+local velocity = CreateClientConVar( "cl_flying_ragdolls_speed", "10", true, false, "Player's dead body flight speed.", 0, 10000 ):GetInt() * 100
+cvars.AddChangeCallback( "cl_flying_ragdolls_speed", function( _, __, value )
+    velocity = ( tonumber( value ) or 10 ) * 100
+end, addonName )
 
 local directions = {
-    [KEY_W] = function( ang )
-        return ang:Forward() * velocity
-    end,
-    [KEY_S] = function( ang )
-        return ang:Forward() * -velocity
-    end,
-    [KEY_D] = function( ang )
-        return ang:Right() * velocity
-    end,
-    [KEY_A] = function( ang )
-        return ang:Right() * -velocity
-    end
+    [ KEY_W ] = function( ang ) return ang:Forward() * velocity end,
+    [ KEY_S ] = function( ang ) return ang:Forward() * -velocity end,
+    [ KEY_D ] = function( ang ) return ang:Right() * velocity end,
+    [ KEY_A ] = function( ang ) return ang:Right() * -velocity end
 }
 
-hook_Add("CreateClientsideRagdoll", "flying_ragdolls", function( pl, ent )
-    if enable and pl:IsPlayer() and (pl:EntIndex() == LocalPlayer():EntIndex()) then
-        local keys = {
-            [KEY_W] = false,
-            [KEY_S] = false,
-            [KEY_A] = false,
-            [KEY_D] = false
-        }
+hook.Add( "CreateClientsideRagdoll", addonName, function( entity, ragdoll )
+    if not enabled:GetBool() then return end
+    if entity:EntIndex() ~= LocalPlayer():EntIndex() then return end
 
-        ent:AddCallback( "PhysicsCollide", function( self, data )
-            if (data["HitSpeed"]:Length() > 1000) then
-                util_Decal("Blood", data["HitPos"] + data["HitNormal"], data["HitPos"] - data["HitNormal"])
+    local phys = ragdoll:GetPhysicsObject()
+    if not IsValid( phys ) then return end
+
+    local keys = {
+        { KEY_W, false },
+        { KEY_S, false },
+        { KEY_A, false },
+        { KEY_D, false }
+    }
+
+    hook.Add( "PlayerButtonDown", phys, function( _, __, keyCode )
+        for _, data in ipairs( keys ) do
+            if data[1] == keyCode then
+                data[2] = true
+                break
             end
-        end)
-
-        hook_Add("PlayerButtonDown", ent, function( self, ply, key )
-            if (ply:EntIndex() == pl:EntIndex()) and (keys[ key ] != nil) then
-                keys[ key ] = true
-            end
-        end)
-
-        hook_Add("PlayerButtonUp", ent, function( self, ply, key )
-            if (ply:EntIndex() == pl:EntIndex()) and (keys[ key ] != nil) then
-                keys[ key ] = false
-            end
-        end)
-
-        local phys = ent:GetPhysicsObject()
-        if IsValid( phys ) then
-            hook_Add("Think", phys, function( self )
-                for key, state in pairs( keys ) do
-                    if ( state == true ) then
-                        local func = directions[ key ]
-                        if ( func == nil ) then continue end
-
-                        self:ApplyForceCenter( func( pl:EyeAngles() ) )
-                    end
-                end
-            end)
         end
-    end
-end)
+    end )
+
+    hook.Add( "PlayerButtonUp", phys, function( _, __, keyCode )
+        for _, data in ipairs( keys ) do
+            if data[1] == keyCode then
+                data[2] = false
+                break
+            end
+        end
+    end )
+
+    hook.Add( "Think", phys, function( self )
+        for _, data in ipairs( keys ) do
+            if not data[2] then continue end
+
+            local func = directions[ data[1] ]
+            if func then
+                self:ApplyForceCenter( func( entity:EyeAngles() ) )
+                break
+            end
+        end
+    end )
+
+    ragdoll:AddCallback( "PhysicsCollide", function( _, data )
+        if not blood:GetBool() then return end
+        if data.HitSpeed:Length() < 1000 then return end
+        util_Decal( "Blood", data.HitPos + data.HitNormal, data.HitPos - data.HitNormal )
+    end )
+end )
